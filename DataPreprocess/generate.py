166d183577201531121b18f47cafe098baea88dd
ysy <yuzi020322@163.com>
2025-07-07 12:24:47 +0800

    convert 428MRA to nii

--- DataPreprocess/generate.py
+++ DataPreprocess/generate.py
@@ -39,52 +39,48 @@ if __name__ == '__main__':
     df = pd.read_excel(mra_path, sheet_name='Report')
     data = []
     n = len(df)
+    tot = -1
     for i in range(n):
-        ta, date, dcm = df['serial_number'][i], df['mra_examination_date'][i], df['DICOM'][i]
+        ta, date, dcm = df['serial_number'][i], df['mra_examination_date'][i], str(df['DICOM'][i])
         date = str(date).split()[0].replace('-', '')
         py = ''.join(c for c in df['pinyin'][i] if c.isupper())
-        data.append([ta, date, dcm, py])
-        print(ta, date, dcm, py)
-        if '+' in str(dcm):
-            continue
-        if dcm in err_list:
-            data[i].append('DICOM error')
-            data[i] += [0, 0, 0, 0]
-            continue
 
-        dcm_folder = root_path + 'DICOM_data/' + str(dcm) + ' DICOM'
-        file_list = list_files(dcm_folder)
-        patient_list = classify(file_list)
-        flag = 0
-        for y in patient_list:
-            for file in y[1]:
-                name = ''.join(c for c in file['PatientName'] if c.isupper())
-                if name == py:
-                    flag = 1
-                    new_date = [file['StudyDate'], file['SeriesDate'], file['AcquisitionDate'], file['ContentDate']]
-                    output_path = root_path + '428MRA/' + str(ta)
-                    if date in new_date:
-                        flag = 2
-                        dicom2nii([y], output_path)
-                break
-        if flag == 0:
-            data[i].append('No name')
-            data[i] += [0, 0, 0, 0]
-        elif flag == 1:
-            data[i].append('Wrong date')
-            data[i] += new_date
-        else:
-            data[i].append('Successful')
-            data[i] += [0, 0, 0, 0]
+        dicom = [int(d) for d in dcm.split('+')]
+        for d in dicom:
+            data.append([ta, date, d, py])
+            tot += 1
+            print(ta, date, d, py)
+            if d in err_list:
+                data[tot].append('DICOM error')
+                data[tot] += [0, 0, 0, 0]
+                continue
 
-        save = pd.DataFrame(data=data, columns=['TA', 'date', 'dcm', 'pinyin', 'status',
-                                                'StudyDate', 'SeriesDate', 'AcquisitionDate', 'ContentDate'])
-        save.to_excel(root_path + '/428MRA/data.xlsx', sheet_name='data')
+            dcm_folder = root_path + 'DICOM_data/' + str(d) + ' DICOM'
+            file_list = list_files(dcm_folder)
+            patient_list = classify(file_list)
+            flag = 0
+            for y in patient_list:
+                for file in y[1]:
+                    name = ''.join(c for c in file['PatientName'] if c.isupper())
+                    if name == py:
+                        flag = 1
+                        new_date = [file['StudyDate'], file['SeriesDate'], file['AcquisitionDate'], file['ContentDate']]
+                        output_path = root_path + '428MRA/' + str(ta)
+                        if date in new_date:
+                            flag = 2
+                            dicom2nii([y], output_path)
+                    break
+            if flag == 0:
+                data[tot].append('No name')
+                data[tot] += [0, 0, 0, 0]
+            elif flag == 1:
+                data[tot].append('Wrong date')
+                data[tot] += new_date
+            else:
+                data[tot].append('Successful')
+                data[tot] += [0, 0, 0, 0]
 
-        # opt = input()
-        # if opt == '1':
-        #     break
-
-
-
+            save = pd.DataFrame(data=data, columns=['TA', 'date', 'dcm', 'pinyin', 'status',
+                                                    'StudyDate', 'SeriesDate', 'AcquisitionDate', 'ContentDate'])
+            save.to_excel(root_path + '/428MRA/data.xlsx', sheet_name='data')
 
