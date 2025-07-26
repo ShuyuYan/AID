fe4b527789bc10c6847ebe4a0f0229fe6fff4649
ysy <yuzi020322@163.com>
2025-07-26 13:45:48 +0800

    add new function to rename the nii files

--- DataPreprocess/tools.py
+++ DataPreprocess/tools.py
@@ -30,7 +30,7 @@ def harmonize_date_format(file_path):
     df.to_excel(file_path, index=False, sheet_name='data')
 
 
-def remove_angle_brackets_in_filenames(root_dir: str):
+def remove_angle_brackets_in_filenames(root_dir):
     """
     遍历root_dir目录及其子目录，将文件名中的'<>'去掉
     """
@@ -39,16 +39,16 @@ def remove_angle_brackets_in_filenames(root_dir: str):
         print(f"错误：目录 {root_dir} 不存在或不是一个文件夹。")
         return
 
-    for path in root_path.rglob("*.nii"):
+    for path in root_path.rglob('*.nii'):
         old_name = path.name
-        new_name = old_name.replace("<", "").replace(">", "")
+        new_name = old_name.replace('<', '').replace('>', '')
         if new_name != old_name:
             new_path = path.with_name(new_name)
             try:
                 path.rename(new_path)
-                print(f"已重命名：\n  {path}\n→ {new_path}\n")
+                print(f'已重命名：\n  {path}\n→ {new_path}\n')
             except Exception as e:
-                print(f"重命名失败：{path}\n原因：{e}\n")
+                print(f'重命名失败：{path}\n原因：{e}\n')
 
 
 if __name__ == '__main__':
