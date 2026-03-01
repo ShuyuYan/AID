735425e3906c97ddecbcffc2d276ef91a9ed06b9
ysy <yuzi020322@163.com>
2026-03-01 10:23:36 +0800

    ROC comparison plot of models trained with varying numbers of modalities

--- TAK/roc_curve.py
+++ TAK/roc_curve.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 """
-使用不同数量模态的模型ROC对比图
+使用不同数量模态数据的模型ROC对比图
 """
 
 y_true = np.load("/home/yanshuyu/Data/AID/results/roc/y_true.npy")   # shape (N,)
@@ -23,8 +23,8 @@ plt.figure(figsize=(7, 6))
 
 for model_name, file in model_files:
     data = np.load(file, allow_pickle=True)
-    y_score = data["y_score"]          # (N, 3)
-    y_score_k = y_score[:, k]          # 只取第 k 类概率
+    y_score = data["y_score"]
+    y_score_k = y_score[:, k]
 
     fpr, tpr, _ = roc_curve(y_true_k, y_score_k)
     roc_auc = auc(fpr, tpr)
@@ -45,5 +45,5 @@ plt.title(f"ROC Curve Treatment A")
 plt.legend(loc="lower right")
 plt.grid(alpha=0.3)
 plt.tight_layout()
-plt.savefig('roc.png', dpi=1000, bbox_inches='tight')
+plt.savefig('roc.svg', dpi=300, bbox_inches='tight')
 plt.show()
